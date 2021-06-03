ca774f3ad6aec6cbb0eff3245d5c42304f5e99bb
BALAKRAM TUDU <48530348+balakram@users.noreply.github.com>
2021-06-03 22:23:06 +0530

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+const $ = el => {
+  return document.querySelector(el);
+};
+
+window.addEventListener("scroll", event => {
+  if (window.pageYOffset > 50) {
+    if ($("nav").classList.contains("active")) return;
+    $("nav").classList.add("active");
+    $("a.scroll-to-top").classList.add("active");
+  } else {
+    $("nav").classList.remove("active");
+    $("a.scroll-to-top").classList.remove("active");
+  }
+});
+
+// prettier-ignore
+$("footer").innerHTML += `<p class="text-center" style="font-size:0.9rem;color:#ffffff80">Copyright © ${new Date().getFullYear()} BTSTUDIOS</p>`;
